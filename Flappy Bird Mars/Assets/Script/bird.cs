692506dd021ad1972bd02c044fa858dcd731a3e3
bode4ok <43826340+bode4ok@users.noreply.github.com>
2020-12-14 00:38:57 +0200

    Flappy-Bird-Mars

---
+++ Flappy Bird Mars/Assets/Script/bird.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    public GameObject gameover;
+    public GameObject skor, emas, perak;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "grd"){
+            GameObject.Find("suara jatuh").GetComponent<AudioSource>().Play();
+            skor.SetActive (false);
+            gameover.SetActive (true);
+            if(PlayerPrefs.GetInt("highscore")<skor.GetComponent<skor>().score){
+                PlayerPrefs.SetInt("highscore", skor.GetComponent<skor>().score);
+            }
+
+            if(skor.GetComponent<skor>().score >= 50){
+                emas.SetActive(true);
+            }else if(skor.GetComponent<skor>().score >= 20){
+                perak.SetActive(true);
+            }
+            Time.timeScale = 0;
+        } 
+
+        if(other.gameObject.name == "addScore"){
+            GameObject.Find("suara skor").GetComponent<AudioSource>().Play();
+            skor.GetComponent<skor> ().score++;
+        }
+    }
+}
---
+++ Flappy Bird Mars/Assets/Script/splash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void masuk(){
+        Application.LoadLevel("play");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
